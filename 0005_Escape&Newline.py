b68f18b8a4b9c42441eb859164b6f73c587362c4
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-07 02:42:10 +0000

    Caracteres escape y nueva linea

---
+++ 0005_Escape&Newline.py
@@ -0,0 +1,6 @@
+#\ caracter de escape breve de la cadena de caracteres
+#n caracter para que la consola tenga nueva linea de salida
+print("La Witsi Witsi Araña\nsubió su telaraña.")
+print()
+print("Vino la lluvia\ny se la llevó.")
+#con los nuevos caracteres \n debe hacer saltos de linea para tener dos renglones separados por el print sin argumento.
