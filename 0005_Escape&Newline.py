931a3ef8efd21ce223c67699a85381fa7501ae68
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-07 03:11:27 +0000

    Caracter de escape y nueva linea

---
+++ 0005_Escape&Newline.py
@@ -0,0 +1,5 @@
+#\ en la serie de caracteres de cadena hara un escape
+#n comando de nueva linea o nueva salida para la consola
+print("La Witsi Witsi Araña\nsubió a su telaraña.")
+print()
+print("Vino la lluvia\ny se la llevó.")
