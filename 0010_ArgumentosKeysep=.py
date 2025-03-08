1786a7d632fc3e23ad46d1a2e10d1e0b7b52eb70
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-08 20:56:35 +0000

    Key word sep=

---
+++ 0010_ArgumentosKeysep=.py
@@ -0,0 +1,2 @@
+print("Mi", "nombre", "es", "Monty", "Python.", sep="-")
+#por lo visto en este tema de key words end= quiere finalizar la cadena de argumentos y sirve para juntarlos y sep= los separa
