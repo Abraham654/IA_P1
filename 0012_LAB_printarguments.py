8fbed0b5669508129f43b53c43b928518b4e4b7d
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-08 21:10:47 +0000

    LAB

---
+++ 0012_LAB_printarguments.py
@@ -0,0 +1,3 @@
+print("Programming", "Essentials", "in", sep="***", end="...")
+print("Python.")
+#como se buscaba solo separar el primer renglon con *** y juntar python del segundo renglon con ... a sep le coloce * y a end .
