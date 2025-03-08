33f7eb12bb44a0f060cd0945cbdd021a9e14c21b
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-08 21:03:11 +0000

    Keys sep y end

---
+++ 0011_KeyWordsep&end.py
@@ -0,0 +1,3 @@
+print("Mi","nombre","es", sep="_", end="*")
+print("Monty","Python.", sep="*", end="*\n")
+#basicamente provoco que un renglon tuviera _ yel otro * y ambos estan separados por un * pero los unifico por end los dos renglones
