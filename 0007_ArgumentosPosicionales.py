2d2f0c4e74c21a50f70bda877b0934b26a5bbe13
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-08 20:29:35 +0000

    Argumentos Posicionales

---
+++ 0007_ArgumentosPosicionales.py
@@ -0,0 +1,6 @@
+print("Mi nombre es", "Python." )
+print("Monthy Python.")
+#esto es forma posicional de este codigo
+#y mi prediccion de impresion en terminal es:
+#Mi nombre es Python.
+#Monthy Python.
