1f2aa7da66a668a748d769481d3dc8c9b7c9b0d5
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-11 16:43:27 +0000

    Cuestionario de seccion pt2

---
+++ 0015_Cuestionariodeseccion2.py
@@ -0,0 +1,5 @@
+#print(sep="&","fish","chips")
+#mi prediccion para este codigo es que imprima &fish&chips o &fish chips es que ahora el sep= esta al inicio
+#despues de ejecutarlo salio error por sintaxis lo cual no previ pero si lo presenti que algo iva pasar fuera de lo normal
+print("fish","chips",sep="&")
+#para que funcione tiene que estar hasta el final y produce un fish$chips
