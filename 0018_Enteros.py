3ecd25fa21f7b8d28c3f70da9cb44a8c20e60e7c
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-11 17:07:06 +0000

    Enteros

---
+++ 0018_Enteros.py
@@ -0,0 +1,5 @@
+#existen numeros enteros
+#existen numeros punto-flotantes o flotantes que estos son fraccionarios
+#para lectura en python no s pueden escribir los numeros con comas si no con guiones_ 11_111_111
+print(0o123) #aqui el numero esta en octal y lo imprime normal en decimal que es 83
+print(0x123) #aqui el numero esta en hexadecimal y lo imprime normal en decimal que es 291
