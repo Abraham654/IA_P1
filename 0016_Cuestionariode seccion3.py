36f0177f7693aeab8366e50dea19e67617412102
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-11 16:54:19 +0000

    Cuestionario de seccion final

---
+++ 0016_Cuestionariode seccion3.py
@@ -0,0 +1,9 @@
+#Pregunta 3: ¿Cuál de las siguientes print() invocaciones de función generarán un SyntaxError?
+print('Greg\'s book.')
+print("'Greg's book.'")
+print('"Greg\'s book."')
+print("Greg\'s book.")
+#print('"Greg's book."')
+#mi impresion por colores que arroja el github antes de ejecutar es el ultimo print, pero creo tambien van a fallar todos los que tienen el \
+#despues de ejecutar el programa marca error en linea 6 por string literal indeterminado
+#segun el curso menciona que' necesita un caracter de escape
