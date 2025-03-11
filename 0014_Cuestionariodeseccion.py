2bcc44dfc84785ca19e522c04d014ec276608056
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-11 16:33:32 +0000

    Cuestionario de seccion

---
+++ 0014_Cuestionariodeseccion.py
@@ -0,0 +1,4 @@
+print("Mi\nnombre\nes\nBond.",end=" ")
+print("James Bond.")
+#Mi prediccion para este codigo es que Mi nombre es Bond., se imprimira de forma de renglon por palabra y en un mismo renglon
+#quedaria Bond. James Bond. ya que el end=" " provoca que se unan en un mismo renglon con un espacio.
