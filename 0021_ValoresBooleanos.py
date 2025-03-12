ef8e328fec2698cb87cfda0b523f19d84afb4de5
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-12 17:32:34 +0000

    Booleanos

---
+++ 0021_ValoresBooleanos.py
@@ -0,0 +1,6 @@
+#hablamos de verdadero y falso representados como 1s y 0s
+#son las detonaciones True y False
+print(True > False)
+print(True < False)#aparentemente le da la razon al mayor que para imprimir
+print(True)
+print(False)
