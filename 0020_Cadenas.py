44b1f479c861c401b5f3a3880584719d5ef81cdc
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-12 17:18:09 +0000

    Cadenas

---
+++ 0020_Cadenas.py
@@ -0,0 +1,6 @@
+print("Me gusta \"Monty Python\"")#el caracter de escape \ permite que se puedan imprimir comillas
+#Lo probe sin esto y arrojo error de syntaxis.
+#print("Me gusta "Monty Python"")
+print('Me gusta "Monty Python"')#otra solucion es usar apostrofes y las comillas para evitar el error.
+print('I\'m Monty Python.')#como lograr que se muestre el apostrofe forma uno.
+print("I'm Monty Python.")#como lograr que se muestre el apostrofe con "'".
