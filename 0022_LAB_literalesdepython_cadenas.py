9ab6d85ea41dc05431a32694f5caec7007509aca
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-12 17:54:01 +0000

    LAB literales-Python-cadenas

---
+++ 0022_LAB_literalesdepython_cadenas.py
@@ -0,0 +1,11 @@
+#el punto es lograr representar "Estoy" ""aprendiendo"" """Python"""
+print('"Estoy"\n""aprendiendo""\n"""Python"""')#Esto lo estoy haciendo sin tantos prints y con los minimos escapes.
+print("\"Estoy\"\n\"\"aprendiendo\"\"\n\"\"\"Python\"\"\"")#Esto es con un print y con todos los escapes.
+#esto es con varios prints pero sin escapes
+print('"Estoy"')
+print('"aprendiendo"')
+print('"Python"')
+#esto es con prints y escapes
+print("\"Estoy\"")
+print("\"\"aprendiendo\"\"")
+print("\"\"\"Python\"\"\"")
