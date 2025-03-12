dfb446a1d09e0d3d1462c827392ae7441770b5c4
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-12 16:21:12 +0000

    Flotantes

---
+++ 0019_Flotantes.py
@@ -0,0 +1,3 @@
+print(0.0000000000000000000001)#este es un numero decimal con muchos ceros por lo cual usa python notacion cientifica para mostrarlo
+print(4.0)#aunque 4.0 basicamente es un 4 entero python por el uso de punto lo considera ya flotante
+print(4e10)#el e tambien provoca que los lea como flotantes.
