4d37340f51c62d118c42d64cf7b8a8749b04aa54
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-15 00:37:12 +0000

    Python como calculadora_suma

---
+++ 0023_Suma.py
@@ -0,0 +1,2 @@
+#se ve que es un codigo que con el print y los enteros que lleva dentro en operacion los imprimira en la terminal como resultado
+print(2+2) #como es 2+2 el resultado es 4 el cual si imprimio en la terminal.
