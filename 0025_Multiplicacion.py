06f858ec6a3be5877ebcea33250bb09d2fc8219d
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-15 00:49:28 +0000

    Calcu Multiplicacion

---
+++ 0025_Multiplicacion.py
@@ -0,0 +1,8 @@
+#en este codigo se seguira usando print, pero como ya se vio en exponeciacion solo con que uno de los numeros
+#este como flotante el resultado estara igual como flotante.
+print(2 * 3)#multiplicacion de enteros igual a 6
+print(2 * 3.)#mutliplicacion de entero y flotante igual a 6.0
+print(2. * 3)#multiplicacion de flotante y entero igual a 6.0
+print(2. * 3.)#multiplicacion de flotantes igual a 6.0
+#extra
+print(2.5*3)
