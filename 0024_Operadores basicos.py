2b5944c975adda6e6f65b007720b62d993b78f20
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-15 00:45:03 +0000

    Calcu Exponenciacion

---
+++ 0024_Operadores basicos.py
@@ -0,0 +1,14 @@
+#los operadores son simbolos por los cuales en programacion se pueden hacer operaciones
+#como se usa print va imprimir el resultado de las operaciones, en este caso ** es exponenciacion
+print(2 ** 3)#aqui ambos numeros son enteros como el quese va a elevar y la elevacion por lo cual saldra 8 entero
+print(2 ** 3.)#aqui la elevacion es flotante y saldra 8.0 por la elevacion flotante, fraccionario o decimal, etc
+print(2. ** 3)#aqui el numero que se va elevar es el flotante y saldra 8.0 por eso mismo.
+print(2. ** 3.)#aqui ambos son flotantes y seguira saliendo 8.0
+#+
+#-
+#*
+#/
+#//
+#%
+#**
+#todos estos son los operadores basicos
