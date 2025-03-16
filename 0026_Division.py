505b5c9f7f9a8dfcfc5d71d9858866f414e4b2c7
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-16 02:48:13 +0000

    Division flotante calcu

---
+++ 0026_Division.py
@@ -0,0 +1,7 @@
+#símbolo de / es un operador de división
+print(6 / 3)
+print(6 / 3.)
+print(6. / 3)
+print(6. / 3.)
+#al ejecutar todos los resuk¿ltados salen como flotantes a pesar de que la linea 2 sean numeros enteros en la operacion
+
