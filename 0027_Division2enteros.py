dddce8f451f8ec5c7f7b25e57b4841d03cd5bdad
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-16 02:54:23 +0000

    Division enteros calcu

---
+++ 0027_Division2enteros.py
@@ -0,0 +1,10 @@
+#Un símbolo de //es un operador de división entera
+print(6 // 3)
+print(6 // 3.)
+print(6. // 3)
+print(6. // 3.)
+print(6.5//2)
+#al ejecutar me percato que la diferencia de la otra division de diagonal simple podra dividir numeros fraccionarios y imprimir
+#los numeros con decimales sin redondear
+#por ejemplo el siguiente que es flotante
+print(6.5/2)
