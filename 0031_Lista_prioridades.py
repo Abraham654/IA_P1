0eb0ba351bfdc34aa1e7a92b4c904b7e3265d526
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-16 07:23:03 +0000

    Prioridades

---
+++ 0031_Lista_prioridades.py
@@ -0,0 +1,10 @@
+#1 ** potenccias
+#2 +,- unuarios nota:los operadores unuarios a la derecha del operador exponencial enlazan con mayor fuerza
+#3 *,/,//,%
+#4 +,-
+print(2 * 3 % 5)#son del mismo nivel prioritario asi que izq a dere
+#Operadores y paréntesis
+print((5 * ((25 % 13) + 100) / (2 * 13)) // 2)#10.0
+print((2 ** 4), (2 * 4.), (2 * 4))#16, 8.0, 8
+print((-2 / 4), (2 / 4), (2 // 4), (-2 // 4))#-0.5, 0.5, 0, -1
+print((2 % -4), (2 % 4), (2 ** 3 ** 2))#-2, 2, 512
