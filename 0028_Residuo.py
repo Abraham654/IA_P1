a94e6b9b03af356c5d2fcd84c3ba510fb240ca43
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-16 03:28:10 +0000

    Residuo calcu

---
+++ 0028_Residuo.py
@@ -0,0 +1,9 @@
+#ahora se vera la funcion que arroja los reciduos de las divisiones %
+print(14 % 4) #resulta en dos ya que si queremos enteros solo cabe 3 veces el 4 en el 12
+print(12 % 4.5)
+#3.0 – no 3 pero 3.0. La regla aun funciona:
+#12 // 4.5 da 2.0,
+#2.0 * 4.5 da 9.0,
+#12 - 9.0 da 3.0.
+#como nota no funciona el dividir entre 0
+#print(1/0)
