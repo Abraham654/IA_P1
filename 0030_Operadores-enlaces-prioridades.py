554763ffeb454c8b8acfe61074410b8a8b4de8b6
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-16 06:22:21 +0000

    Operadores enlaces

---
+++ 0030_Operadores-enlaces-prioridades.py
@@ -0,0 +1,9 @@
+print(2 + 3 * 5)#parece que python respeta el orden de signos matematicos ya que priorizo la multiplicacion sobre la suma
+#operador sobre enlace dando lectura principalmente de izq a der
+print(9 % 6 % 2)#9/6=1 con residuo 3, 3/2=1 residuo 1
+print(2 ** 2 ** 3)#para potencia respeta el orden de primero elevar las potencias para tener la potencia final y elevar el numero
+print(-3 ** 2)#-9 por que no contempla el signo
+print((-3)**2)#9 por que esta contemplanto todo con signo para elevar
+print(-2 ** 3)#-8
+print(-(3 ** 2))#-9 ya que al final hay multiplicacion de signos
+
