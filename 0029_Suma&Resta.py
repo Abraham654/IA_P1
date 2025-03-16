b82b43449bc515f84f205562c0d8873b93f937e7
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-16 03:36:59 +0000

    Suma y Resta calcu

---
+++ 0029_Suma&Resta.py
@@ -0,0 +1,7 @@
+print(-4 + 4)#suma de numeros enteros pero pues el primero es negativo y sale 0
+print(-4. + 8)#suma de numeros con uno negativo pero flotante y por eso sale 4.0
+#operador de resta -
+print(-4 - 4)#basicamente suma de negativos entera -8
+print(4. - 8)
+print(-1.1)#solo lo va imprimir -1.1
+print(+2)#solo imprimir 2
