2b09b50ffb13022e7d1f38deedf5355fd2fb0bbb
Abraham654 <143459589+Abraham654@users.noreply.github.com>
2025-03-17 05:15:16 +0000

    Emplear variable

---
+++ 0033_Emplearvariable.py
@@ -0,0 +1,5 @@
+var = 1 #asigna valor 1 a variable con =
+account_balance = 1000.0 #asigna valor flotante a la variable con =
+client_name = 'John Doe' #asigna texto a variable pero ocupa ''
+print(var, account_balance, client_name) #imprime tadas las variables en una cadena
+print(var) #vuelve a imprimir solo el var
